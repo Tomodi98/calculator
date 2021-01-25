e3b82fa57c1f02d6d882b0221f75f498b284d6d9
Antonio Somodi <somodi.antonio@gmail.com>
2021-01-25 21:34:15 +0100

    add basic functionality

--- app.js
+++ app.js
@@ -37,11 +37,52 @@ const operations = (() => {
 
 const numberButtons = document.getElementById('numberButtons').children;
 const display = document.getElementById('display');
+let firstNumber = '';
+let secondNumber = '';
+let operatorClicked = false;
+let operation;
 
+//add function to numbers
 for (let i = 0; i < numberButtons.length; i++) {
-    numberButtons[i].onclick = function(){clickNumber(this)}
+    numberButtons[i].onclick = function(){clickNumber(this)};
 }
 
 function clickNumber (e) {
     display.textContent += e.textContent;
+    if (!operatorClicked) {
+        firstNumber += e.textContent;
+    }
+    else {
+        secondNumber += e.textContent;
+    }
+}
+const operatorButtons = document.getElementById('operatorButtons').children;
+//add function to operators
+for (let i = 0; i < operatorButtons.length -1; i++) {     // -1 so it excludes the '='
+    operatorButtons[i].onclick = function(){clickOperator(this)};
+}
+function clickOperator (e) {
+    display.textContent += e.textContent;
+    operation = e.textContent;
+    operatorClicked = true;
+}
+
+const equalsSign = document.getElementById('operate');
+equalsSign.onclick = function(){calculate()};
+
+function calculate () {
+    display.textContent = operations.operate(operation, firstNumber, secondNumber);
+    //reset everything
+    firstNumber = display.textContent;
+    secondNumber = '';
+    operatorClicked = false
+}
+//clears and resets everything
+const clearButton = document.getElementById('clear');
+clearButton.onclick = function(){clear()};
+function clear () {
+    display.textContent = '';
+    firstNumber = '';
+    secondNumber = '';
+    operatorClicked = false
 }
