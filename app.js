61ea084d6e77918f0715c4e648f039fe9e93a222
Antonio Somodi <somodi.antonio@gmail.com>
2021-02-02 00:02:39 +0100

    the funny message when dividing by 0 deletes after 2 seconds now

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const operations = (() => {
             return Number(x) * Number(y);
         }
         else if (operator === '/') {
-            setTimeout(function(){clear()},2000);
+            setTimeout(function(){clear()},2*1000);
             if (Number(y) === 0) {
                 return 'Hey! You can\'t divide by 0!';
             }
